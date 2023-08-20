d01bfba8b50432305ad1b09f8d95920df5a74488
SandroMaglione <lass.maglio@gmail.com>
2023-08-20 18:25:42 +0200

    signal on object collide

--- entities/player.gd
+++ entities/player.gd
@@ -9,36 +9,6 @@ func _ready():
 func _process(_delta):
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	$GridMovement.move(input_direction)
-	
-#	moving_animation(input_direction)
-			
-#func moving_animation(input_direction: Vector2) -> void:
-#	var animation_state: StringName = animation
-#	var moving_direction: Vector2 = $GridMovement.moving_direction
-#	var vectorDirection = vector2Direction(moving_direction)
-#
-#	if moving_direction.length() > 0:
-#		animation_state = "walk_" + vectorDirection
-#	else:
-#		if input_direction.length() > 0:
-#			vectorDirection = vector2Direction(input_direction)
-#			animation_state = "idle_" + vectorDirection
-#		else:
-#			vectorDirection = animation_state.get_slice("_", 1)
-#			animation_state = "idle_" + vectorDirection
-			
-#	play(animation_state)
- 
-#func vector2Direction(vec: Vector2) -> String:
-#	var direction = "down"
-#	if vec.y > 0: direction = "down"
-#	elif vec.y < 0: direction = "up"
-#	elif vec.x > 0:
-#		flip_h = false
-#		direction = "right"
-#	elif vec.x < 0:
-#		# Horizontal flip since we have one animation for both left and right walking and idle
-#		flip_h = true
-#		direction = "right"
-#
-#	return direction
+
+func _on_grid_movement_collided(body):
+	print(body)
--- scripts/grid_movement.gd
+++ scripts/grid_movement.gd
@@ -3,14 +3,17 @@ extends RayCast2D
 @export var actor: Node2D
 @export var speed: float = 0.25
 
+signal collided(body)
+
 var moving_direction: Vector2 = Vector2.ZERO
+var collided_direction: Vector2 = Vector2.ZERO
 
 func _ready():
 	# Set movement direction as DOWN by default
 	self.target_position = Vector2.DOWN * Constants.TILE_SIZE
  
 func move(direction: Vector2) -> void:
-	if moving_direction.length() == 0 && direction.length() > 0:
+	if moving_direction.length() == 0 and direction.length() > 0:
 		var movement = Vector2.DOWN
 		if direction.y > 0: movement = Vector2.DOWN
 		elif direction.y < 0: movement = Vector2.UP
@@ -22,6 +25,7 @@ func move(direction: Vector2) -> void:
 		
 		# Allow movement only if no collision in next tile
 		if !self.is_colliding():
+			collided_direction = Vector2.ZERO
 			moving_direction = movement
 			
 			var new_position = actor.global_position + (moving_direction * Constants.TILE_SIZE)
@@ -29,3 +33,8 @@ func move(direction: Vector2) -> void:
 			var tween = create_tween()
 			tween.tween_property(actor, "position", new_position, speed).set_trans(Tween.TRANS_LINEAR)
 			tween.tween_callback(func(): moving_direction = Vector2.ZERO)
+		else:
+			if collided_direction != direction:
+				# Collide and emit signal only once
+				collided_direction = direction
+				collided.emit(self.get_collider())
