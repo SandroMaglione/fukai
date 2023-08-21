745a0b339987a7bea34c7c8f51cb127634cbca0a
SandroMaglione <lass.maglio@gmail.com>
2023-08-21 06:06:21 +0200

    collect item inside Tilemap

--- entities/player.gd
+++ entities/player.gd
@@ -1,5 +1,7 @@
 extends Node2D
 class_name Player
+
+@onready var grid_movement: GridMovement = $GridMovement
  
 func _ready():
 	position = position.snapped(Vector2.ONE * Constants.TILE_SIZE)
@@ -7,13 +9,24 @@ func _ready():
  
 func _process(_delta):
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	$GridMovement.move(input_direction)
+	var collider = grid_movement.move(input_direction)
+	
+	if collider == null:
+		grid_movement.execute_move(input_direction)
 
-func _on_grid_movement_collided(body):
+func _on_grid_movement_collided(body, movement):
 	print(body)
-	if body is CollectItem:
-		body.on_collect_item()
-	elif body is Stairs:
+	if body is TileMap:
+		var coords = body.local_to_map(global_position + movement * Constants.TILE_SIZE)
+		var tile_data = body.get_cell_tile_data(1, coords)
+		if tile_data != null:
+			var collect_item_resource = tile_data.get_custom_data("collect_item_resource")
+			if collect_item_resource is CollectItemResource:
+				print(collect_item_resource)
+		
+	if body is Stairs:
 		body.complete_tier()
+		grid_movement.execute_move(movement)
 	elif body is Coin:
 		body.collect_coin()
+		grid_movement.execute_move(movement)
--- scripts/grid_movement.gd
+++ scripts/grid_movement.gd
@@ -1,48 +1,52 @@
 extends RayCast2D
+class_name GridMovement
 
 @export var actor: Node2D
 @export var speed: float = 0.25
 
-signal collided(body)
+signal collided(body, movement)
 
 var moving_direction: Vector2 = Vector2.ZERO
 var collided_direction: Vector2 = Vector2.ZERO
 
 func _ready():
-	# Set movement direction as DOWN by default
 	self.target_position = Vector2.DOWN * Constants.TILE_SIZE
  
-func move(direction: Vector2) -> void:
-	if moving_direction.length() == 0 and direction.length() > 0:
-		var movement = Vector2.DOWN
-		if direction.y > 0: movement = Vector2.DOWN
-		elif direction.y < 0: movement = Vector2.UP
-		elif direction.x > 0: movement = Vector2.RIGHT
-		elif direction.x < 0: movement = Vector2.LEFT
-		
+func move(direction: Vector2) -> Object:
+	if direction.length() > 0:
+		var movement = _unit_direction(direction)
+
 		self.target_position = movement * Constants.TILE_SIZE
-		self.force_raycast_update() # Update the `target_position` immediately
+		self.force_raycast_update()
 		
 		var collider = self.get_collider()
-		
-		# Stop only when colliding with "wall"
-		if collider == null:
-			_execute_move(movement)
-		elif collider is CollisionObject2D:
-			if collider.collision_mask != 2:
-				_execute_move(movement)
+		if collider != null:
+			if collided_direction != movement:
+				# Collide and emit signal only once
+				collided_direction = movement
+				collided.emit(self.get_collider(), movement)
 				
-				if collided_direction != direction:
-					# Collide and emit signal only once
-					collided_direction = direction
-					collided.emit(self.get_collider())
+			return collider
 
-func _execute_move(movement: Vector2) -> void:
-	collided_direction = Vector2.ZERO
-	moving_direction = movement
-	
-	var new_position = actor.global_position + (moving_direction * Constants.TILE_SIZE)
-	
-	var tween = create_tween()
-	tween.tween_property(actor, "position", new_position, speed).set_trans(Tween.TRANS_LINEAR)
-	tween.tween_callback(func(): moving_direction = Vector2.ZERO)
+	return null
+
+func execute_move(direction: Vector2) -> void:
+	if moving_direction.length() == 0 and direction.length() > 0:
+		var movement = _unit_direction(direction)
+		
+		collided_direction = Vector2.ZERO
+		moving_direction = movement
+		
+		var new_position = actor.global_position + (moving_direction * Constants.TILE_SIZE)
+		
+		var tween = create_tween()
+		tween.tween_property(actor, "position", new_position, speed).set_trans(Tween.TRANS_LINEAR)
+		tween.tween_callback(func(): moving_direction = Vector2.ZERO)
+
+func _unit_direction(direction: Vector2) -> Vector2:
+	var movement = Vector2.DOWN
+	if direction.y > 0: movement = Vector2.DOWN
+	elif direction.y < 0: movement = Vector2.UP
+	elif direction.x > 0: movement = Vector2.RIGHT
+	elif direction.x < 0: movement = Vector2.LEFT
+	return movement
