13fdc6a5c8c62a6fa06efb75b88a1fa4decfb23a
SandroMaglione <lass.maglio@gmail.com>
2023-08-21 16:37:19 +0200

    working on walls in A* path

--- entities/chase_enemy_movement.gd
+++ entities/chase_enemy_movement.gd
@@ -2,7 +2,6 @@ extends State
 class_name ChaseEnemyMovement
 
 @export var actor: Node2D
-
 @export var grid_movement: GridMovement
 
 var tilemap: TileMap
@@ -11,14 +10,22 @@ var player: Node2D
 var astar_grid: AStarGrid2D
 
 func _ready():
-	player = get_tree().get_nodes_in_group("player")[0]
-	tilemap = get_tree().get_nodes_in_group("tilemap")[0]
+	player = get_tree().get_nodes_in_group("player")[0] as Node2D
+	tilemap = get_tree().get_nodes_in_group("tilemap")[0] as TileMap
+	
 	
 	astar_grid = AStarGrid2D.new()
+	
 	astar_grid.region = tilemap.get_used_rect()
 	astar_grid.cell_size = Vector2.ONE * Constants.TILE_SIZE
 	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+	
 	astar_grid.update()
+	
+	var walls = tilemap.get_used_cells(Constants.WALLS_LAYER_ID)
+	for cell in walls:
+		print(cell)
+		astar_grid.set_point_solid(cell)
 	
 func Physics_update(_delta):
 	if TurnBasedMovement.is_enemy_turn() and not grid_movement.is_moving():
@@ -33,9 +40,16 @@ func next_move_direction() -> Vector2:
 	var player_coords = tilemap.local_to_map(player.global_position)
 	
 	var path_ids = astar_grid.get_id_path(actor_coords, player_coords)
-	var next_direction = next_direction_from_path(actor_coords, path_ids[1])
+	print(path_ids)
+	
+	var next_path_id = path_ids[1]
+	print(astar_grid.is_point_solid(next_path_id))
+	
+	var next_direction = next_direction_from_path(actor_coords, next_path_id)
+	
 	
 	if path_ids.size() == 2:
+		_on_grid_movement_movement_completed()
 		print("Attack!", next_direction)
 		return Vector2.ZERO
 	else:
--- scripts/constants.gd
+++ scripts/constants.gd
@@ -1,6 +1,7 @@
 extends Node
 
 const TILE_SIZE: int = 8
+const WALLS_LAYER_ID: int = 0
 const COLLECT_ITEM_LAYER_ID: int = 1
 const COINS_LAYER_ID: int = 2
 const STAIRS_LAYER_ID: int = 3
