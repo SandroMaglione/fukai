e51371666bb40c8d294221da1a6d27826d7ef9bd
SandroMaglione <lass.maglio@gmail.com>
2023-08-24 16:58:07 +0200

    added player camera and stats texture

--- ui/select_inventory_ui.gd
+++ ui/select_inventory_ui.gd
@@ -113,3 +113,6 @@ func _on_enter_dungeon_button_pressed():
 
 func _on_craft_potions_button_pressed():
 	get_tree().change_scene_to_file("res://ui/craft_potions_ui.tscn")
+
+func _on_player_stats_button_pressed():
+	get_tree().change_scene_to_file("res://ui/player_stats_ui.tscn")
--- ui/single_player_stat_ui.gd
+++ ui/single_player_stat_ui.gd
@@ -4,6 +4,12 @@ class_name SinglePlayerStatUI
 signal selected_assign_stat()
 
 @onready var points_assigned_label = %PointsAssignedLabel
+@onready var stat_texture = %StatTexture
+
+@export var stat_texture_source: Texture2D
+
+func _ready():
+	stat_texture.texture = stat_texture_source
 
 var points_assigned: int = 0:
 	set(value):
